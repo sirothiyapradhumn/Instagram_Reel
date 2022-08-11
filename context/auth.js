c15d0255a6274d6f46a227fdd041479c1638cb91
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-08-11 20:25:38 +0530

    :rocket: firebase setup + authWrapper

---
+++ context/auth.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function AuthWrapper() {
+    console.log("hello u are in auth wrapper");
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default AuthWrapper
---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth} from "firebase/auth";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGCd4csupemv2J-Dylfv9nNa2rg6uLK6o",
+  authDomain: "insta-reel-my.firebaseapp.com",
+  projectId: "insta-reel-my",
+  storageBucket: "insta-reel-my.appspot.com",
+  messagingSenderId: "988031305226",
+  appId: "1:988031305226:web:5e9db829b1530be7ce41f3",
+  measurementId: "G-MXTV9ZXB8N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+export{ auth };
