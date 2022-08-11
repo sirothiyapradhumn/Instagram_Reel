73b5d3864bc04af21ac3464eeadedf3a78b84274
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-08-11 18:53:41 +0530

    :fire: UI complete

--- components/Feed.js
+++ components/Feed.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Upload from './Upload'
+import Avatar from '@mui/material/Avatar';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
 function Feed() {
   return (
     <div className='feed-container'>
@@ -9,6 +12,16 @@ function Feed() {
         <div className='videos-container'>
             <div className='post-container'>
                 <video />
+                <div className='video-info'>
+                    <div className='avatar-container'>
+                    <Avatar alt="Semy Sharp" src="/static/images/avatar/2.jpg" sx={{margin:'0.5rem'}}/>
+                    <p>Sahil</p>
+                    </div>
+                    <div className='post-like'>
+                        <FavoriteIcon/>
+                        <p>10</p>
+                    </div>
+                </div>
             </div>
 
             <div className='post-container'>
